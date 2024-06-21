31e5c8928fddacf9558cf6498e6e408d3c21bfe0
Laurenca <108622056+Laurenca@users.noreply.github.com>
2024-06-21 15:11:10 +0200

    add text formatting to body

--- src/arbeidsavtalepane/htmlBody.ts
+++ src/arbeidsavtalepane/htmlBody.ts
@@ -279,7 +279,7 @@ externallyFundedText = `Ansettelsesforholdet er knyttet til eksternt finansiert 
   }
 
   let bodyIntro =
-    "<p>" +
+    `<p class="MsoNormal">` +
     tempEmployeeText +
     educationalCompetenceNeeded +
     aremalText +
@@ -291,9 +291,12 @@ externallyFundedText = `Ansettelsesforholdet er knyttet til eksternt finansiert 
     abroardEmployeeText +
     "</p>";
 
+
+
+
   return `
     ${bodyIntro}
-        <p>
+        <p class="MsoNormal">
             Arbeidsavtalen inneholder, sammen med eventuell utlysningstekst, 
             ansettelsesvilkårene ved tiltredelsen. Ansettelsen skjer med plikt 
             til å rette seg etter de bestemmelser som til enhver tid gjelder for stillingen.  
@@ -304,22 +307,22 @@ externallyFundedText = `Ansettelsesforholdet er knyttet til eksternt finansiert 
             tjenestetvistloven, Hovedavtalen i staten (HA) med Tilpasningsavtale ved UiB, 
             personalreglement samt eventuelle særavtaler og retningslinjer som gjelder for stillingen. 
         </p>
-        <p>
+        <p class="MsoNormal">
             Lønn utbetales den 12. hver måned via bank, med mindre annet er avtalt særskilt. 
             Det trekkes 2% pensjonsinnskudd til medlemskap i Statens pensjonskasse for stillinger med minimum 20 prosent av full stilling. 
             Staten og hovedsammenslutningene har gjennom særavtale fastsatt at arbeidstakere i 100 % stilling skal trekkes kr 400,- i bruttolønn pr år, 
             som inngår i delfinansiering av opplærings- og utviklingstiltak, 
         </p>
-        <p>
+        <p class="MsoNormal">
             Reglene i statsansatteloven § 15 om prøvetid gjelder. Prøvetiden er 6 måneder fra tiltredelse.
             Dersom den ansatte har vært fraværende fra arbeidet i prøvetiden, kan arbeidsgiver forlenge prøvetiden med en periode som tilsvarer lengden av fraværet.  
         </p>
-        <p>
+        <p class="MsoNormal">
             Arbeidstiden og lengden av pauser følger reglene i aml, HTA og særavtaler. 
             Den alminnelige arbeidstiden skal i gjennomsnitt ikke overstige 37,5 timer pr. uke i 100 % stilling. 
             Arbeidstiden skal i den utstrekning det er mulig, legges i tidsrommet mellom kl. 07.00 og kl. 17.00 og fordeles på 5 dager pr. uke. 
         </p>
-        <p>
+        <p class="MsoNormal">
             Rett til ferie og feriepenger reguleres i henhold til ferieloven, HTA og særavtale. 
             Reglene for fastsettelse av ferietidspunktet følger av ferieloven § 6. Arbeidstaker kan kreve at hovedferie som omfatter tre uker 
             gis i hovedferieperioden 1 juni - 30 september. 
@@ -328,25 +331,25 @@ externallyFundedText = `Ansettelsesforholdet er knyttet til eksternt finansiert 
             For å motivere eldre arbeidstakere til å stå lenger i arbeid, gis, i henhold til HTA, for tiden 
             tjenestefri med lønn tilsvarende 10 dager pr. år fra det kalenderåret man fyller 62 år.
         </p>
-        <p>
+        <p class="MsoNormal">
             Arbeidstaker har rett til full lønn under sykdom i inntil ett år, permisjon ved svangerskap, fødsel, adopsjon og amming, og ved barns sykdom iht. HTA §§ 18.- 20. 
             Når viktige velferds- og omsorgsgrunner foreligger, kan en arbeidstaker tilstås velferdspermisjon med lønn i inntil 12 arbeidsdager. 
             Arbeidstakere gis anledning til nødvendig korttidsfravær i arbeidstiden, f.eks. 
             korte lege- eller tannlegebesøk. Korttidsfraværet må avklares med nærmeste overordnede.
         </p>
-        <p>
+        <p class="MsoNormal">
             UiB tilbyr kompetanseutvikling i henhold til HA med tilpasningsavtale, særavtale og interne retningslinjer.
         </p>
-        <p>
+        <p class="MsoNormal">
             For ansettelsesforholdet gjelder oppsigelsesfrister i statsansatteloven § 22. 
             Ansatte må levere sin oppsigelse skriftlig. Ved oppsigelse fra arbeidsgiver vises det til saksbehandlingsreglene i statsansatteloven § 32.
         </p>
-        <p>
+        <p class="MsoNormal">
             Ansatte må ikke inneha ekstraerverv og annet erverv i strid i staten. Noen ansatte må registrere sine ekstraerverv i henhold til retningslinjer ved UiB. 
             Rettigheter til forsknings- og arbeidsresultater er regulert i Reglement om håndtering av ansattes rettigheter 
             til forsknings- og arbeidsresultater ved Universitetet i Bergen.
         </p>
-        <p>
+        <p class="MsoNormal">
             Ansatte ved UiB har taushetsplikt etter forvaltningsloven, og eventuelt særlige taushetspliktsregler som gjelder for stillingen. 
             Ved å underskrive arbeidskontrakten erklærer den ansatte å kjenne til og respekterer reglene om taushetsplikt.
         </p>
